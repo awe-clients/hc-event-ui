e29fdc7c9d7d13d547c11b976e55c2a3464f3032
Cesimar Xavier <jetompson@gmail.com>
2026-03-03 22:14:35 -0300

    feat: site da caminha da mãe potiguar

---
+++ index.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Caminhada Mãe Potiguar 2024</title>
+    <!-- Fontes -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <!-- Folhas de Estilo -->
+    <link rel="stylesheet"  href="assets/css/caminhada.css">
+    <link rel="stylesheet"  href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+  
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="icon" href="assets/img/favicons/favicon.png">
+    <meta name="theme-color" content="#f7941d">
+    <!-- Icone -->
+    <script src="https://kit.fontawesome.com/e65fa423df.js" crossorigin="anonymous"></script>
+
+    <!-- Scritps do projeto -->
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+
+        <header class="header sticky-top"  style="z-index: 2000;background: #FFFBD6">
+            <nav class="navbar navbar-expand-lg"  style="backdrop-filter: blur(10px);">
+                <section class="container"> 
+                <!-- Logo -->
+                    <a class="navbar-brand" href="#">                       
+                        <img src="assets/img/logo-horizontal.svg" class="img-fluid" alt="logo">
+                    </a>
+                    <!-- Mobile Navbar -->
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                    data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                <!-- Desktop Navbar -->
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul class="navbar-nav fw-bold fs-6">
+                            <li class="nav-item me-3">
+                                <a class="nav-link" href="#sobre">Sobre o evento</a>
+                            </li>
+                            <li class="nav-item me-3">
+                                <a class="nav-link" href="https://drive.google.com/drive/folders/1uoGNeRB6O3XQYRSF2tPQA9O7Hcgf2oJg" target="_blank">Fotos</a>
+                            </li>
+                            <!--
+                            <li class="nav-item me-3">
+                                <a href="https://inscricao.caminhadadamaepotiguar.com.br" class="btn btn-outline-primary text-body rounded-pill fw-bold text-light">Realizar inscrição</a>
+                            </li>
+                            -->
+                        </ul>
+                    </div>
+                </section>
+            </nav>
+        </header>
+      
+          
+        <main class="main">
+
+            <!-- seção de destaque principal -->
+            <section class="section-hero d-flex align-items-baseline">
+                <div class="container h-100">
+                    <div class="row h-100">
+                        <div class="col-12 text-light text-center d-flex flex-column align-items-baseline justify-content-center">
+                            <h1 class="display-4 fw-bold lh-1 mb-5 animate__animated animate__fadeInUp">Comemore o mês das mães com alegria e solidariedade.</h1>
+                            <p class="fs-5 w-100 animate__animated animate__fadeInUp animate__slow">
+                                <strong class="d-block mb-2">Vem caminhar com a gente!</strong>  
+                                <a href="https://inscricao.caminhadadamaepotiguar.com.br" style="display: none;" class="btn btn-primary btn-lg p-3 px-4 bg-destaque">PARTICIPE!</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="destaque py-5">
+                <section class="container py-5">
+                    <div class="row">
+                        <div class="col-lg-10 offset-lg-2">
+                            <h2 class="display-6 pb-5 fw-bold text-uppercase">A CAMINHADA DA MÃE POTIGUAR É UM EVENTO QUE CELEBRA AS MÃES.</h2>
+                            <p class="fs-5 text-black-50">Participe da <strong class="color-destaque">Caminhada da Mãe Potiguar</strong> e celebre com a gente o mês das mães. Contribua para um futuro mais saudável, apoie a amamentação e o bem-estar de toda a família. Seu engajamento é essencial para tornar esse lindo evento ainda mais alegre e solidário. </p>
+
+                        </div>
+                    </div>
+                </section>
+            </section>
+
+            <section class="py-5 section-destaque" id="sobre">
+                <div class="container py-5">
+                    <div class="row py-5">
+                        <div class="col-lg-6 col-md-12 text-light fw-normal">
+                            
+                            <h2 class="pb-4 fw-bold text-uppercase">BUSCAMOS PROMOVER A SAÚDE DA MULHER E DO BEBÊ.</h2>
+                            <p>
+                                A Caminhada da Mãe Potiguar celebra o mês das mães com uma bela manifestação de amor e união. O evento acontece no dia 03 de maio, a partir das 14h, no Arena das Dunas. O roteiro terá 2km, iniciando no Estádio Arena das Dunas e dando uma volta no Centro Administrativo do Estado. No palco do evento, teremos um show com a renomada atração nacional, Renato e Seus Blue Caps.
+                                </p>
+                            <p>
+                                Este evento é uma iniciativa do Governo do Estado do RN, por meio do Corpo de Bombeiros Militar do Rio Grande do Norte e da Secretaria de Saúde do Estado, com o apoio de vários parceiros. Neste ano a expectativa é de reunir 10.000 participantes, arrecadando mais de 10 toneladas de alimentos. Não é apenas uma caminhada, mas um movimento que carrega profundas reflexões sobre a saúde da mulher e do bebê, além de estimular a amamentação e reforçar a vital importância da doação de leite materno, nutrindo a cadeia de apoio que sustenta os bancos de leite do Estado e fortalecendo as bases para uma sociedade mais saudável.
+                            </p>
+
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4 col-md-5 col-12">
+                            <a href="https://inscricao.caminhadadamaepotiguar.com.br" style="opacity: 0;" class="btn btn-outline-primary bg-destaque p-3 d-block fw-bold text-light">Venha fazer parte!</a>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="destaque mb-5 py-5" id="fotos">
+                <section class="container">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12">
+                            <h2 class="fs-3 fw-bold text-uppercase pb-2">VENHA CELEBRAR O DIA DAS MÃES CONOSCO</h2>
+                            <p class="fs-6 text-black-50">Traga sua família e vamos juntos comemorar o mês das mães!</p>
+                        </div>
+                    </div>
+                </section>
+                <section class="container-fluid">
+                    <div class="row g-2">
+                    <?php  
+    
+                        $dir    = 'assets/img/fotos';
+                        $logos = scandir($dir);
+                        unset($logos[0]);
+                        unset($logos[1]);
+    
+                        foreach ($logos as $value) : ?>
+    
+                        <div class="col-md-3 col-6">
+                            <img src="assets/img/fotos/<?= $value ?>" class="img-fluid" alt="">
+                        </div>
+    
+                        <?php endforeach; ?>
+                    </div>
+
+                </section>
+            </section>
+
+            <section class="destaque pb-5">
+                <section class="container py-5">
+                    <!-- <h2 class="fs-3 pb-5 fw-bold text-uppercase">Nossos parceiros acreditam</h2> -->
+                    <div class="row">
+                        <h4 class="fs-4 col-12 my-4 pt-5">Realização</h4>
+                        <?php  
+
+                        $dir    = 'assets/img/realizacao';
+                        $logos = scandir($dir);
+                        unset($logos[0]);
+                        unset($logos[1]);
+                        
+                        foreach ($logos as $value) : ?>
+
+                        <div class="col-md-3 col-6 mt-4 text-center flex-row-reverse">
+                            <img src="assets/img/realizacao/<?= $value ?>" class="img-fluid" height="100" style="max-height: 150px" alt="">
+                        </div>
+
+                        <?php endforeach; ?>
+
+                        <h4 class="fs-4 col-12 my-4 pt-5">Parceiros</h4>
+                        <?php  
+
+                        $dir    = 'assets/img/parceiros';
+                        $logos = scandir($dir);
+                        unset($logos[0]);
+                        unset($logos[1]);
+
+                        
+                        foreach ($logos as $value) : ?>
+
+                        <div class="col-lg-3 col-md-4 col-6 mt-5 text-center">
+                            <img src="assets/img/parceiros/<?= $value ?>" class="img-fluid" height="60" alt="">
+                        </div>
+
+                        <?php endforeach; ?>
+
+
+                    </div>
+                </section>
+            </section>
+
+        </main>
+
+        <footer class="py-3 text-center bg-dark">
+            <a href="http://hcsports.com.br" target="_blank" class="text-light text-decoration-none">HC Sports</a>
+        </footer>
+
+        
+
+        <!-- Modal -->
+        <div class="modal modal-lg fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" style="z-index: 3000;">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Regularize seu licenciamento no Detran</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                     <img loading="lazy" src="assets/img/campanha-ipva.png" class="img-fluid" alt="Pop-up da campanha do meu veículo legal: campanha do detran" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+
+        <script type="text/javascript">
+            
+            $(document).ready(function(){
+               // $("#staticBackdrop").modal('show');
+            });
+        </script>
+
+    </body>
+</html>
